acb57239aaffc72076520a7c376cc71836d7698f
Dylan Richards <dylanrichards@Dylans-MBP.home>
2018-06-16 22:52:35 -0400

    Initial commit

---
+++ ideaboard-api/db/seeds.rb
@@ -0,0 +1,22 @@
+puts "Creating ideas..."
+ideas = Idea.create(
+  [
+    {
+      title: "A new cake recipe",
+      body: "Made of chocolate"
+    },
+    {
+      title: "A Twittern client idea",
+      body: "Only for replying to mentions and DMs"
+    },
+    {
+      title: "A novel set in Italy",
+      body: "A mafia crime drama starring Berlusconi"
+    },
+    {
+      title: "Card game design",
+      body: "Like Uno but involves drinking"
+    },
+  ]
+)
+puts "Ideas created"
